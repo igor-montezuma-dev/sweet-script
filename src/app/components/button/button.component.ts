46aa5d74cba0703f274f7e9f4014354a3c53c2ad
IgorMontezuma20 <igorminranda17@gmail.com>
2024-10-21 22:20:15 -0300

    feat: Implement incrementation/decrement in button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
 
+  addedToCart = false;
+  count = 1;
+
+  addToCart() {
+    this.addedToCart = true;
+  }
+
+  decrement() {
+    if (this.count === 1) {
+      this.addedToCart = false;
+    } else {
+      this.count--;
+    }
+  }
+
+  increment() {
+    this.count++;
+  }
 }
